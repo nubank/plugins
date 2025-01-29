ee4def6fe9bcdb11f77439507557fa7f4ec15552
Carolina Carvalho <carolinalimacarvalho1@gmail.com>
2025-01-28 22:07:24 -0300

    Fork 2.2.19 version

--- packages/video_player/video_player/lib/src/closed_caption_file.dart
+++ packages/video_player/video_player/lib/src/closed_caption_file.dart
@@ -1,9 +1,14 @@
-// Copyright 2020 The Chromium Authors. All rights reserved.
+// Copyright 2013 The Flutter Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+import 'package:flutter/foundation.dart' show objectRuntimeType;
+
 import 'sub_rip.dart';
+import 'web_vtt.dart';
+
 export 'sub_rip.dart' show SubRipCaptionFile;
+export 'web_vtt.dart' show WebVTTCaptionFile;
 
 /// A structured representation of a parsed closed caption file.
 ///
@@ -15,6 +20,7 @@ export 'sub_rip.dart' show SubRipCaptionFile;
 ///
 /// See:
 /// * [SubRipCaptionFile].
+/// * [WebVTTCaptionFile].
 abstract class ClosedCaptionFile {
   /// The full list of captions from a given file.
   ///
@@ -31,11 +37,12 @@ class Caption {
   ///
   /// This is not recommended for direct use unless you are writing a parser for
   /// a new closed captioning file type.
-  const Caption(
-      {required this.number,
-      required this.start,
-      required this.end,
-      required this.text});
+  const Caption({
+    required this.number,
+    required this.start,
+    required this.end,
+    required this.text,
+  });
 
   /// The number that this caption was assigned.
   final int number;
@@ -52,12 +59,16 @@ class Caption {
 
   /// A no caption object. This is a caption with [start] and [end] durations of zero,
   /// and an empty [text] string.
-  static const Caption none =
-      Caption(number: 0, start: Duration.zero, end: Duration.zero, text: '');
+  static const Caption none = Caption(
+    number: 0,
+    start: Duration.zero,
+    end: Duration.zero,
+    text: '',
+  );
 
   @override
   String toString() {
-    return '$runtimeType('
+    return '${objectRuntimeType(this, 'Caption')}('
         'number: $number, '
         'start: $start, '
         'end: $end, '
